cea839774127e595e6adb987fc6b95f421719486
Shuangyi Chen <32474491+shuangyichen@users.noreply.github.com>
2020-03-25 10:38:58 +0800

    Add files via upload

--- SearchinRotatedSortedArray.py
+++ SearchinRotatedSortedArray.py
@@ -1,29 +1,33 @@
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
+    def search(self, nums: List[int], target: int) -> int:
         if len(nums)==0:
-            return [-1,-1]
+            return -1
         elif len(nums)==1:
-            if target == nums[0]:return [0,0]
-            else:return [-1,-1]
+            if target==nums[0]:
+                return 0
+            else:
+                return -1
+        middle = int(len(nums)/2)
         left = 0
-        right = len(nums)
+        right = len(nums)-1
         while left<right:
             mid = int((left+right)/2)
-            if target == nums[mid]:right=mid
-            elif target < nums[mid]:right=mid
-            elif target > nums[mid]:left=mid+1
-            if left==right:
-                if left==len(nums):
-                    return [-1,-1]
-                elif nums[left]!=target:
-                    return [-1,-1]
-        leftr = 0
-        rightr = len(nums)
-        while leftr<rightr:
-            mid = int((leftr+rightr)/2)
-            if target == nums[mid]:leftr=mid+1
-            elif target < nums[mid]:rightr=mid
-            elif target > nums[mid]:leftr=mid+1
-
-
-        return [left,rightr-1]
+            if target==nums[left]:return left
+            elif target==nums[right]:return right
+            elif target==nums[mid]:return mid
+            if left==mid or mid==right:
+                return -1
+            if nums[mid]>nums[right]:#左边是顺序的
+                if target>=nums[left] and target<=nums[mid]:#在左边
+                    left = left
+                    right = mid
+                else:
+                    left = mid
+                    right = right
+            else:#右边是顺序的
+                if target>nums[mid] and target<nums[right]:#在右边
+                    left = mid
+                    right = right
+                else:
+                    left = left
+                    right = mid
